cd2debc334a6cccdf8d9910680f33ecb1734797e
slvthg <slv.thiago.dev@gmail.com>
2026-02-01 19:54:50 -0300

    feat(app): enable global validation pipe and CORS

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import 'dotenv/config';
 
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+
+import { env } from '@shared/config/env';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: env.allowedOrigins,
+    methods: ['GET', 'HEAD', 'PUT', 'POST', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
