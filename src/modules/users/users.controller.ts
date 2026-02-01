599e979157f31aa836a131fd93614324ca7e495c
slvthg <slv.thiago.dev@gmail.com>
2026-02-01 00:10:29 -0300

    refactor(modules/users): extracting userId from the request

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,4 +1,6 @@
-import { Body, Controller, Get } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+
+import { ActiveUserId } from '@shared/decorators/ActiveUserId';
 
 import { UsersService } from './users.service';
 
@@ -7,7 +9,7 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get('/me')
-  me(@Body() userId: string) {
+  me(@ActiveUserId() userId: string) {
     return this.usersService.getUserById(userId);
   }
 }
