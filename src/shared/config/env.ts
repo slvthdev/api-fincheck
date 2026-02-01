3d0eef5734665c132d4f7ef4df11742438ca3c82
slvthg <slv.thiago.dev@gmail.com>
2026-02-01 19:46:17 -0300

    feat(env): validation of the allowedOrigins variable

--- src/shared/config/env.ts
+++ src/shared/config/env.ts
@@ -1,5 +1,13 @@
-import { plainToInstance } from 'class-transformer';
-import { IsNotEmpty, IsString, NotEquals, validateSync } from 'class-validator';
+import { Transform, plainToInstance } from 'class-transformer';
+import {
+  ArrayNotEmpty,
+  IsNotEmpty,
+  IsString,
+  IsUrl,
+  NotEquals,
+  ValidateIf,
+  validateSync,
+} from 'class-validator';
 
 class Env {
   @IsString()
@@ -10,9 +18,27 @@ class Env {
   @IsString()
   @IsNotEmpty()
   dbURL: string;
+
+  @ValidateIf((_, value) => value !== '*')
+  @IsString({ each: true })
+  @IsUrl({ require_tld: false, require_protocol: true }, { each: true })
+  @ArrayNotEmpty()
+  @IsNotEmpty()
+  @Transform(({ value }) => {
+    if (value === '*') {
+      return value;
+    }
+
+    return String(value)
+      .split(';')
+      .map((origin) => origin.trim())
+      .filter(Boolean);
+  })
+  allowedOrigins: string | string[];
 }
 
 export const env: Env = plainToInstance(Env, {
+  allowedOrigins: process.env.ALLOWED_ORIGINS,
   dbURL: process.env.DATABASE_URL,
   jwtSecret: process.env.JWT_SECRET,
 });
